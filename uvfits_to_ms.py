e8b85b110901617ec80821ce79e64fceeeb68b33
e-koch <koch.eric.w@gmail.com>
2017-12-13 15:03:36 -0700

    Convert all spectral resolutions to an MS

--- uvfits_to_ms.py
+++ uvfits_to_ms.py
@@ -9,50 +9,58 @@ from astropy.io import fits
 # The dishes are also all set to the same position, but the baselines
 # are correct in the actual data.
 
-ms_names = []
+# Loop through the different spectral resolutions Jonathan made
+str_prefix = ['M33-ARM-', 'M33-ARM05-', 'M33-ARM1-', 'M33-ARM13-', 'M33-ARM2-',
+              'M33-ARMcont-']
 
-for i in range(1, 89):
+for pref in str_prefix:
 
-    print("On {} of 88".format(i))
+    print("On {}".format(pref))
 
-    # str_name = 'M33-ARM-{}'.format(i)
-    str_name = 'M33-ARM05-{}'.format(i)
-    # str_name = 'M33-ARM1-{}'.format(i)
+    ms_names = []
 
-    uvfits = "calib/uvfits/{}.uvfits".format(str_name)
-    vis_name = "meas_sets/{}.ms".format(str_name)
+    for i in range(1, 89):
 
+        print("On {} of 88".format(i))
 
-    hdu = fits.open(uvfits, mode='update')
-    # For some reason, some of the uvfits don't have the correct CRVAL for
-    # RA and DEC. The correct ones are set as OBSRA and OBSDEC. Make sure
-    # these are the same before importing to CASA
-    hdu[0].header['CRVAL5'] = hdu[0].header['OBSRA']
-    hdu[0].header['CRVAL6'] = hdu[0].header['OBSDEC']
-    hdu.flush()
-    hdu.close()
+        str_name = '{}{}'.format(pref, i)
 
-    importuvfits(fitsfile=uvfits, vis=vis_name)
+        uvfits = "calib/uvfits/{}.uvfits".format(str_name)
+        vis_name = "meas_sets/{}.ms".format(str_name)
 
-    # Change the field name
-    vishead(vis="meas_sets/{}.ms".format(str_name), mode='put',
-            hdkey='field', hdvalue=str_name)
+        hdu = fits.open(uvfits, mode='update')
+        # For some reason, some of the uvfits don't have the correct CRVAL for
+        # RA and DEC. The correct ones are set as OBSRA and OBSDEC. Make sure
+        # these are the same before importing to CASA
+        hdu[0].header['CRVAL5'] = hdu[0].header['OBSRA']
+        hdu[0].header['CRVAL6'] = hdu[0].header['OBSDEC']
+        hdu.flush()
+        hdu.close()
 
-    # Add in rest freq and vsys
-    hdu = fits.open(uvfits)
+        importuvfits(fitsfile=uvfits, vis=vis_name)
 
-    tb.open("{}/SOURCE".format(vis_name), nomodify=False)
-    tb.putcol('REST_FREQUENCY', [[hdu[0].header['RESTFREQ']]])
-    tb.putcol('SYSVEL', [[hdu[0].header['VELO-LSR']]])
-    tb.putcol('NAME', [[str_name]])
-    tb.close()
+        # Change the field name
+        vishead(vis="meas_sets/{}.ms".format(str_name), mode='put',
+                hdkey='field', hdvalue=str_name)
 
-    hdu.close()
+        # Add in rest freq and vsys
+        hdu = fits.open(uvfits)
 
-    ms_names.append(vis_name)
+        tb.open("{}/SOURCE".format(vis_name), nomodify=False)
+        tb.putcol('REST_FREQUENCY', [[hdu[0].header['RESTFREQ']]])
+        tb.putcol('SYSVEL', [[hdu[0].header['VELO-LSR']]])
+        tb.putcol('NAME', [[str_name]])
+        tb.close()
 
-# Now concatenate
+        hdu.close()
 
-# concat(vis=ms_names, concatvis='meas_sets/M33-ARM.ms')
-concat(vis=ms_names, concatvis='meas_sets/M33-ARM05.ms')
-# concat(vis=ms_names, concatvis='meas_sets/M33-ARM1.ms')
+        ms_names.append(vis_name)
+
+    # Now concatenate
+
+    concat(vis=ms_names, concatvis='meas_sets/{}.ms'.format(pref[:-1]))
+
+    # Delete the individual MSs
+    for ms_name in ms_names:
+        rmtables(ms_name)
+
